698276f87e58419db50a853e8a5c09ac1ce88d9c
Ankush <128672958+uic19bca1130@users.noreply.github.com>
2023-06-06 19:13:57 +0530

    new

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using Azure.Messaging.ServiceBus;
 using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using FluentValidation.Results;
+using Azure;
+using Microsoft.AspNetCore.Components.Forms;
 using Scrapping_Linkdin.Models.Request;
-using Scrapping_Linkdin.Validator;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped<IValidator<LinkdinProfile>,LinkdinProfileValidator>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 IConfiguration configuration = new ConfigurationBuilder()
@@ -15,29 +19,60 @@ IConfiguration configuration = new ConfigurationBuilder()
        .AddJsonFile("appsettings.json")
        .Build();
 
+
+
 builder.Services.AddSingleton<ServiceBusClient>(serviceProvider =>
 {
     string connectionString = configuration.GetConnectionString("ServiceBusConnection");
     return new ServiceBusClient(connectionString);
 });
+builder.Services.AddTransient<IValidator<LinkdinProfile>, LinkdinProfileValidator>();
+
+
+
 var app = builder.Build();
 
-//Configure the HTTP request pipeline.
+
+
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
-app.MapPost("/sendlink", async (ServiceBusClient client, IConfiguration configuration, [Microsoft.AspNetCore.Mvc.FromBody] LinkdinProfile linkModel) =>
+
+
+var summaries = new[]
 {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapPost("/sendlink", async (ServiceBusClient client, IConfiguration configuration, [FromBody] LinkdinProfile linkModel, IValidator<LinkdinProfile> validator) =>
+{
+    ValidationResult validationResult = validator.Validate(linkModel);
+    if (!validationResult.IsValid)
+    {
+        var messages = new List<string>();
+        foreach (ValidationFailure failure in validationResult.Errors)
+        {
+            messages.Add(failure.ErrorMessage);
+        }
+        return Results.BadRequest(messages);
+        //  return Results.BadRequest(validationResult.Errors);
+    }
     string queueName = configuration.GetValue<string>("QueueName");
     string partitionKey = linkModel.PartitionKey;
     string linkedInProfileLink = linkModel.ProfileId;
 
+
+
     await using ServiceBusSender sender = client.CreateSender(queueName);
+
+
 
     // Create a Service Bus message with the LinkedIn profile link
     ServiceBusMessage message = new ServiceBusMessage(linkedInProfileLink)
@@ -45,43 +80,18 @@ app.MapPost("/sendlink", async (ServiceBusClient client, IConfiguration configur
         PartitionKey = partitionKey
     };
     try
-
     {
         await sender.SendMessageAsync(message);
     }
     catch (Exception)
     {
-
-
         throw;
     }
     // Send the message to the queue
 
-
     // Return a JSON response
-    return new { Message = "LinkedIn profile link sent to Azure Service Bus" };
+    return Results.Ok(new { message12 = "LinkedIn profile link sent to Azure Service Bus" });
 });
 
-app.MapGet("/getresponse", async (ServiceBusClient client, IConfiguration configuration) =>
-{
-    string queueName = configuration.GetValue<string>("ReceiveName");
-
-    await using ServiceBusReceiver receiver = client.CreateReceiver(queueName);
-
-    ServiceBusReceivedMessage responseMessage = await receiver.ReceiveMessageAsync();
-    if (responseMessage != null)
-    {
-        string response = responseMessage.Body.ToString();
-        //Complete the response message to remove it from the queue
-        await receiver.CompleteMessageAsync(responseMessage);
-
-        // Return the response as JSON
-        return new { Data = response };
-    }
-    else
-    {
-        return new { Data = "No response available" };
-    }
-});
 
 app.Run();
--- Validator/LinkdinProfilevalidator.cs
+++ Validator/LinkdinProfilevalidator.cs
@@ -1,14 +1,20 @@
 ﻿using FluentValidation;
 using Scrapping_Linkdin.Models.Request;
 
-namespace Scrapping_Linkdin.Validator
+public class LinkdinProfileValidator : AbstractValidator<LinkdinProfile>
 {
-    public class LinkdinProfileValidator:AbstractValidator<LinkdinProfile>
+    public LinkdinProfileValidator()
     {
-        public LinkdinProfileValidator()
-        {
-                RuleFor(x=>x.ProfileId).NotNull().NotEmpty();
-        }
-
+        RuleFor(x => x.PartitionKey).NotEmpty();
+        RuleFor(x => x.ProfileId).NotEmpty().Must(BeAValidUrl).WithMessage("Invalid URL format");
     }
-}
+
+
+
+    private bool BeAValidUrl(string url)
+    {
+        // Implement your custom URL validation logic here
+        // Return true if the URL is valid; otherwise, return false
+        return Uri.TryCreate(url, UriKind.Absolute, out _);
+    }
+}   
